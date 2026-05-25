85665fd6148a3c38865ceb84420d4ae08d6dbd06
Konstantin <kbogatyrev@gmail.com>
2026-05-24 21:46:06 -0700

    Final(?) version.

--- ProperNounDBMerge.py
+++ ProperNounDBMerge.py
@@ -3,7 +3,6 @@ import sqlite3
 import sys
 
 def merge_proper_nouns (db_cursor):
-
     select_query = """
         SELECT hw.id,         -- 0
         hw.source, 
@@ -76,7 +75,6 @@ def merge_proper_nouns (db_cursor):
 
             old_descriptor_id = pn_row[21]
             old_inflection_id = pn_row[22]
-
 
             print(pn_row)
 
@@ -168,7 +166,6 @@ def merge_proper_nouns (db_cursor):
     # ----------------------------------------------------------------------------------------
 
             if (spade_dict['spade_mark']):
-
                 spade_insert_query = """
                     INSERT INTO M.proper_nouns_spade 
                     (
@@ -496,6 +493,125 @@ def merge_second_locative (db_cursor, old_inflection_id, new_inflection_id):
     except Exception as e:
         print('Exception: %s, %s' % (sys.exc_info()[0], e))
 
+def update_second_part_id (db_cursor, old_2nd_part_id, new_second_part_id):
+    second_part_query = f"""
+        UPDATE M.descriptor 
+        SET second_part_id={new_second_part_id} 
+        WHERE second_part_id = {old_2nd_part_id}; 
+    """
+    try:
+        db_cursor.execute (second_part_query)
+    except IOError as io_ex:
+        print('IO Error.', io_ex.args[0])
+    except sqlite3.Error as sqlite_ex:
+        print('sqlite3 error: ', sqlite_ex.args[0])
+    except Exception as e:
+        print('Exception: %s, %s' % (sys.exc_info()[0], e))
+
+def merge_compounds(db_cursor):
+    select_query = """
+        SELECT hw.id,                -- 0
+               hw.source, 
+               hw.comment, 
+               hw.usage, 
+               hw.variant, 
+               hw.variant_comment, 
+               hw.see_ref, 
+               hw.second_part,       -- 7 
+               d.id,
+               i.id
+        FROM headword hw JOIN descriptor d ON hw.id=d.word_id JOIN inflection i ON i.descriptor_id=d.id 
+        WHERE d.id IN 
+            (SELECT DISTINCT 
+                old_d.second_part_id
+                FROM headword old_hw 
+                    JOIN descriptor old_d ON old_hw.id=old_d.word_id  
+                    JOIN inflection old_i ON old_i.descriptor_id=old_d.id
+                    JOIN proper_nouns old_pn ON old_pn.word_id=old_hw.id
+                    JOIN M.headword new_hw ON old_hw.source=new_hw.source
+                    JOIN M.descriptor new_d ON (old_d.graphic_stem=new_d.graphic_stem
+                        AND new_d.main_symbol=old_d.main_symbol
+                        AND new_d.inflection_type=old_d.inflection_type)
+                WHERE old_d.second_part_id > 0
+                ORDER BY new_hw.source
+            );
+        """
+
+    hw_insert_query = """
+        INSERT INTO M.headword 
+        (
+            source,
+            plural_of,
+            comment,
+            usage,
+            variant,
+            variant_comment,
+            see_ref,
+            back_ref,
+            source_entry_id,
+            spryazh_sm,
+            second_part,
+            is_edited
+        )
+        VALUES ( ?,?,?,?,?,?,?,?,?,?,?,? );
+    """
+
+    try:
+        hw_dict = {}
+
+        db_cursor.execute(select_query)
+        hw_rows = db_cursor.fetchall()
+        for hw_row in hw_rows:
+            old_headword_id = hw_row[0]
+            hw_dict['source'] = hw_row[1]
+            hw_dict['plural_of'] = ''
+            hw_dict['comment'] = hw_row[2]
+            hw_dict['usage'] = hw_row[3]
+            hw_dict['variant'] = hw_row[4]
+            hw_dict['variant_comment'] = hw_row[5]
+            hw_dict['see_ref'] = hw_row[6]
+            hw_dict['back_ref'] = ''
+            hw_dict['spryazh_sm'] = 0
+            hw_dict['second_part'] = hw_row[7]
+            hw_dict['is_edited'] = 0
+            old_2nd_part_id = hw_row[8]
+            old_inflection_id = hw_row[9]
+
+            data = (
+                hw_dict['source'],
+                '',
+                hw_dict['comment'],
+                hw_dict['usage'],
+                hw_dict['variant'],
+                hw_dict['variant_comment'],
+                hw_dict['see_ref'],
+                hw_dict['back_ref'],
+                -1,
+                0,
+                hw_dict['second_part'],
+                0
+            )
+
+            db_cursor.execute (hw_insert_query, data)
+            new_headword_id = db_cursor.lastrowid
+            merge_stress (db_cursor, old_headword_id, new_headword_id)
+            merge_homonyms (db_cursor, old_headword_id, new_headword_id)
+            new_second_part_id = merge_descriptor (db_cursor, old_headword_id, new_headword_id)
+            update_second_part_id (db_cursor, old_2nd_part_id, new_second_part_id)
+            new_inflection_id = merge_inflection (db_cursor, old_2nd_part_id, new_second_part_id)
+            merge_common_deviation (db_cursor, old_inflection_id, new_inflection_id)
+            merge_difficult_forms (db_cursor, old_inflection_id, new_inflection_id)
+            merge_missing_forms (db_cursor, old_inflection_id, new_inflection_id)
+            merge_second_locative (db_cursor, old_inflection_id, new_inflection_id)
+
+    except IOError as io_ex:
+        print('IO Error.', io_ex.args[0])
+    except sqlite3.Error as sqlite_ex:
+        print('sqlite3 error: ', sqlite_ex.args[0])
+    except Exception as e:
+        print('Exception: %s, %s' % (sys.exc_info()[0], e))
+
+
 #def merge_incomplete_parses (db_cursor):
 #    ip_insert_query = f"""
 #        INSERT INTO M.incomplete_parses
@@ -517,9 +633,14 @@ if __name__ == "__main__":
     sql_attach = "ATTACH DATABASE '../Zal-Data/ZalData/ZalData_Master.db3' AS 'M';"
     db_cursor.execute (sql_attach)
 
-    merge_proper_nouns (db_cursor)
+#    merge_proper_nouns (db_cursor)
+
+#    db_connection.commit()
+
+    merge_compounds (db_cursor)
+
+    db_connection.commit()
 
 #    merge_incomplete_parses (db_cursor)
 
-    db_connection.commit()
 
